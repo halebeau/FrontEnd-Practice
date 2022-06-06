172a1e9207d6bb9b2efe0db0438eef92112fd23c
Beau <halebeau3@gmail.com>
2022-06-06 16:49:17 -0500

    Added: js script to html file, removed js file

--- button-change/index.js
+++
@@ -1,8 +0,0 @@
-const btn = document.getElementById("btn")
-
-btn.addEventListener("click", function onClick() {
-  btn.style.backgroundColor = "red"
-  btn.style.borderColor = "red"
-  btn.style.color = "white"
-  icon.style.color = 'white'
-})
