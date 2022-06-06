8aa031ba34eb5383e128ddc133aad3d7188d17b8
Beau <halebeau3@gmail.com>
2022-06-06 16:46:22 -0500

    Added: some css to js file

--- button-change/index.js
+++ button-change/index.js
@@ -1,14 +1,8 @@
-// const btn = document.geElementById("btn")
-// btn.addEventListener("click", function onCLick(e) {
-//   btn.style.backgroundColor = "red"
-//   btn.style.color = "white"
-// })
-
-
 const btn = document.getElementById("btn")
 
 btn.addEventListener("click", function onClick() {
   btn.style.backgroundColor = "red"
+  btn.style.borderColor = "red"
   btn.style.color = "white"
   icon.style.color = 'white'
 })
